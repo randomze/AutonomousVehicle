cc5f81fadacf366cd6a76b9c39f0f9524bf69f16
Pedro Taborda <33559524+PedroTaborda@users.noreply.github.com>
2022-01-14 15:52:46 +0000

    Added object collision through SAT

--- matlab_utils/get_block_edges.m
+++ matlab_utils/get_block_edges.m
@@ -1,7 +1,8 @@
-function edges = get_edges(block,scale)
+function edges = get_block_edges(block,scale)
 %GET_EDGES Returns de x and y coordinates of the adjacent vertexes of the block
-    edges = zeros(5, 2);
-    [x, y] = block(:);
+    edges = zeros(4, 2);
+    x = block(1);
+    y = block(2);
     edges(1,:) = [x, y];
     edges(2,:) = [x + scale, y];
     edges(3,:) = [x + scale, y + scale];
---
+++ matlab_utils/is_colliding.m
@@ -0,0 +1,45 @@
+function colliding = is_colliding(obj1, obj2)
+%IS_COLLIDING Checks if obj1 and obj2 are colliding with separating axis theorem
+    % obj1 and obj2 must be convex, and described by adjacent vertexes (ANTI-CLOCKWISE)
+    % (meaning obj1(1) is assumed to be connected only to obj1(2) and
+    % obj1(end)
+
+    % Maybe AABB followed by SAT would be faster - if performance is an
+    % issue - due to most objects not colliding with each other
+
+    for i = 1:length(obj1)
+        j = mod(i,length(obj1)) + 1;
+        x1 = obj1(i, 1);
+        y1 = obj1(i, 2);
+        x2 = obj1(j, 1);
+        y2 = obj1(j, 2);
+        dx = x2 - x1;
+        dy = y2 - y1;
+
+        % find axis to project onto (perpendicular to edge)
+        projection_axis = [dy, -dx]/norm([dy,dx]);
+
+        % find projection of obj1 onto axis
+        obj1_projection = dot(obj1, repmat(projection_axis, size(obj1, 1), 1), 2);
+
+        % find projection of obj2 onto axis
+        obj2_projection = dot(obj2, repmat(projection_axis, size(obj2, 1), 1), 2);
+
+        % find "shadow" of projections
+        p1m = min(obj1_projection);
+        p1M = max(obj1_projection);
+        p2m = min(obj2_projection);
+        p2M = max(obj2_projection);
+
+        % if projections don't overlap, a separating axis was found, 
+        % so objects are not colliding
+        if p1M < p2m || p1m > p2M
+            colliding = false;
+            return
+        end
+    end
+    % if we get here, then for all edges, there was no separating axis
+    colliding = true;
+
+end
+
