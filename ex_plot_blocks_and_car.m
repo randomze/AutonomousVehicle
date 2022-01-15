56b577a4b9b8554557882e288685d395443d0852
Pedro Taborda <33559524+PedroTaborda@users.noreply.github.com>
2022-01-15 18:16:40 +0000

    changed example script to reflect new changes

--- ex_plot_blocks_and_car.m
+++ ex_plot_blocks_and_car.m
@@ -1,18 +1,17 @@
 
-x = 370;
-y = -315;
-window = 100;
+x = -15;
+y = 60;
+window = 50;
 
 % generate and plot car
 figure(1); 
 clf;
 car = car_repr(x, y, pi/4, pi/6, L, Lr, Lf, d, r, 0.1); 
-plot_car(car); 
-axis equal;
+plot_car(car, {'-b', 'LineWidth', 1.2});
 
 % generate and plot blocks
 blcks = load_blocks(road_edges, meters_per_pixel); 
-ad_blocks = admissible_blocks(blcks, [x-window, x+window], [y-window, y+window]); 
+ad_blocks = admissible_blocks(blcks, [x-window/2, x+window/2], [y-window/2, y+window/2]); 
 plot_blocks(ad_blocks, meters_per_pixel); 
 axis equal;
 
