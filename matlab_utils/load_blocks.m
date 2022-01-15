c440dbbaf0d3f47edcf5aacae706b3b570e0065b
Pedro Taborda <33559524+PedroTaborda@users.noreply.github.com>
2022-01-15 18:16:36 +0000

    world reference frame now centered on image center

--- matlab_utils/load_blocks.m
+++ matlab_utils/load_blocks.m
@@ -10,12 +10,17 @@ function blocks = load_blocks(edge_binary_img, scale)
 
     % Find the 1-valued pixels
     [y, x] = find(edge_binary_img);
+    
+    % Get information to center image
+    img_sz = size(edge_binary_img);
+    center_x = img_sz(2)/2;
+    center_y = img_sz(1)/2;
 
     % For each 1-valued pixel, create the corresponding block
     for i = 1:n_blocks
         % Create the block with the xy coordinates, inverting y coordinate
         % to keep usual (x - left, y - up) coordinate system
-        blocks(i, :) = scale*[x(i), -y(i)];
+        blocks(i, :) = scale*[(x(i)-center_x), -(y(i)-center_y)];
     end
 
 end
--- matlab_utils/plot_car.m
+++ matlab_utils/plot_car.m
@@ -1,25 +1,13 @@
-function plot_car(car)
+function plot_car(car, plot_args)
 %PLOT_CAR Summary of this function goes here
 %   Detailed explanation goes here
+    if nargin < 2
+        plot_args = {'b'};
+    end
     car_size = size(car);
-    color = 'b';
     hold on;
     for i = 1:car_size(1)
-        plot(car(i, :, 1), car(i, :, 2), color);
-        plot([car(i, 1, 1), car(i, end, 1)], [car(i, 1, 2), car(i, end, 2)], color);
+        plot([car(i, :, 1), car(i, 1, 1)], [car(i, :, 2), car(i, 1, 2)], plot_args{:});
     end
 end
 
-% function plot_car(car)
-% %PLOT_CAR Summary of this function goes here
-% %   Detailed explanation goes here
-%     car_size = size(car);
-%     hold on;
-%     for i = 1:car_size(1)
-%         for j = 1:car_size(2)-1
-%             plot([car(i, j, 1), car(i, j+1, 1)], [car(i, j, 2), car(i, j+1, 2)]);
-%         end
-%         plot([car(i, 1, 1), car(i, end, 1)], [car(i, 1, 2), car(i, end, 2)]);
-%     end
-% end
-% 
